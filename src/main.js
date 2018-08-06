22de3e30455d5c13f36b7dd23bad6d1ecab921a2
duanqz <duanqz@gmail.com>
2018-08-06 11:52:08 +0800

    Do not use typescript

---
+++ src/main.js
@@ -0,0 +1,21 @@
+
+var Crawler = require("crawler");
+
+var c = new Crawler({
+    maxConnections : 10,
+    // This will be called for each crawled page
+    callback : function (error, res, done) {
+        if (error) {
+            console.log(error);
+        } else {
+            var $ = res.$;
+            // $ is Cheerio by default
+            //a lean implementation of core jQuery designed specifically for the server
+            console.log($("title").text());
+        }
+        done();
+    }
+});
+
+// Queue just one URL, with default callback
+c.queue('http://www.baidu.com');
