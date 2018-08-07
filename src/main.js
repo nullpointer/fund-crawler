976623ef5e8efaf85b12d6d65db0d732863b7298
duanqz <duanqz@gmail.com>
2018-08-07 14:10:43 +0800

    Parse data

--- src/main.js
+++ src/main.js
@@ -1,21 +1,60 @@
 
-var Crawler = require("crawler");
+const Crawler = require("crawler");
 
-var c = new Crawler({
+const c = new Crawler({
     maxConnections : 10,
     // This will be called for each crawled page
     callback : function (error, res, done) {
         if (error) {
-            console.log(error);
+            console.error(error);
         } else {
-            var $ = res.$;
-            // $ is Cheerio by default
-            //a lean implementation of core jQuery designed specifically for the server
-            console.log($("title").text());
+            parse(res.body)
         }
         done();
     }
 });
 
+
+function parse(data) {
+    const start = data.indexOf('{');
+    const end = data.indexOf('}') + 1;
+    const jsonStr = data.slice(start, end);
+    const jsonObj = eval('(' + jsonStr + ')');
+
+    const fundItems = [];
+    for (const key in jsonObj) {
+        for (const item of jsonObj[key]) {
+            let fundItem = {};
+            const fundArray = item.split('|');
+            fundItem['code'] = fundArray[0];
+            fundItem['name'] = fundArray[1];
+            fundItem['day'] = fundArray[3];
+            fundItem['unitNetWorth'] = fundArray[4];
+            fundItem['dayOfGrowth'] = fundArray[5];
+            fundItem['recent1Week'] = fundArray[6];
+            fundItem['recent1Month'] = fundArray[7];
+            fundItem['recent3Month'] = fundArray[8];
+            fundItem['recent6Month'] = fundArray[9];
+            fundItem['recent1Year'] = fundArray[10];
+            fundItem['recent2Year'] = fundArray[11];
+            fundItem['recent3Year'] = fundArray[12];
+            fundItem['fromThisYear'] = fundArray[13];
+            fundItem['fromBuild'] = fundArray[14];
+            fundItem['serviceCharge'] = fundArray[18];
+            fundItem['upEnoughAmount'] = fundArray[24];
+            fundItems.push(fundItem);
+        }
+    }
+    console.log(fundItems);
+}
+
+
 // Queue just one URL, with default callback
-c.queue('http://www.baidu.com');
+c.queue({
+    uri: 'https://fundapi.eastmoney.com/fundtradenew.aspx',
+    ft: 'pg',
+    sc: '1',
+    st: 'desc',
+    pi: '2',
+    pn: '3000'
+});
