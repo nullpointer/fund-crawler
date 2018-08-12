010379fc6b2e190701c689bc0b512a68b502cfb9
duanqz <duanqz@gmail.com>
2018-08-12 11:06:51 +0800

    Fix parser

--- src/fundparser.js
+++ src/fundparser.js
@@ -9,23 +9,22 @@ exports.parse = function parse(data) {
     const fundItems = [];
     for (const item of jsonObj['datas']) {
         let fundItem = {};
-        const fundArray = item.split('|');
+        const fundArray = item.split(',');
         fundItem['code'] = fundArray[0];
         fundItem['name'] = fundArray[1];
         fundItem['day'] = fundArray[3];
         fundItem['unitNetWorth'] = fundArray[4];
-        fundItem['dayOfGrowth'] = fundArray[5];
-        fundItem['recent1Week'] = fundArray[6];
-        fundItem['recent1Month'] = fundArray[7];
-        fundItem['recent3Month'] = fundArray[8];
-        fundItem['recent6Month'] = fundArray[9];
-        fundItem['recent1Year'] = fundArray[10];
-        fundItem['recent2Year'] = fundArray[11];
-        fundItem['recent3Year'] = fundArray[12];
-        fundItem['fromThisYear'] = fundArray[13];
-        fundItem['fromBuild'] = fundArray[14];
-        fundItem['serviceCharge'] = fundArray[18];
-        fundItem['upEnoughAmount'] = fundArray[24];
+        fundItem['dayOfGrowth'] = fundArray[6];
+        fundItem['recent1Week'] = fundArray[7];
+        fundItem['recent1Month'] = fundArray[8];
+        fundItem['recent3Month'] = fundArray[9];
+        fundItem['recent6Month'] = fundArray[10];
+        fundItem['recent1Year'] = fundArray[11];
+        fundItem['recent2Year'] = fundArray[12];
+        fundItem['recent3Year'] = fundArray[13];
+        fundItem['fromThisYear'] = fundArray[14];
+        fundItem['fromBuild'] = fundArray[15];
+        fundItem['serviceCharge'] = fundArray[20];
         fundItems.push(fundItem);
     }
     return fundItems;
