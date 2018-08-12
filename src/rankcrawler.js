a801a1162f3c81f241a038f9d61c30b89183363f
duanqz <duanqz@gmail.com>
2018-08-12 14:31:26 +0800

    Refactor

--- src/rankcrawler.js
+++ src/rankcrawler.js
@@ -24,22 +24,14 @@ const c = new Crawler({
 exports.start = function start() {
     const rankUri = 'http://fund.eastmoney.com/data/rankhandler.aspx?op=ph&dt=kf&ft=%s&rs=&gs=0&sc=zzf&st=desc&pi=1&pn=10000&dx=1'
 
-    // 全部
-    c.queue({ uri: Util.format(rankUri, 'all'), type: 'all' });
-    // 股票型
-    c.queue({ uri: Util.format(rankUri, 'gp'), type: 'gupiao' });
-    // 混合型
-    c.queue({ uri: Util.format(rankUri, 'hh'), type: 'hunhe' });
-    // 债券型
-    c.queue({ uri: Util.format(rankUri, 'zq'), type: 'zhaiquan' });
-    // 指数型
-    c.queue({ uri: Util.format(rankUri, 'zs'), type: 'zhishu' });
-    // QDII
-    c.queue({ uri: Util.format(rankUri, 'qdii'), type: 'qdii' });
-    // LOF
-    c.queue({ uri: Util.format(rankUri, 'lof'), type: 'lof' });
-    // FOF
-    c.queue({ uri: Util.format(rankUri, 'fof'), type: 'fof' });
+    c.queue({ uri: Util.format(rankUri, 'all'), type: 'all' }); // 全部
+    c.queue({ uri: Util.format(rankUri, 'gp'), type: 'gupiao' }); // 股票型
+    c.queue({ uri: Util.format(rankUri, 'hh'), type: 'hunhe' }); // 混合型
+    c.queue({ uri: Util.format(rankUri, 'zq'), type: 'zhaiquan' }); // 债券型
+    c.queue({ uri: Util.format(rankUri, 'zs'), type: 'zhishu' }); // 指数型
+    c.queue({ uri: Util.format(rankUri, 'qdii'), type: 'qdii' }); // QDII
+    c.queue({ uri: Util.format(rankUri, 'lof'), type: 'lof' }); // LOF
+    c.queue({ uri: Util.format(rankUri, 'fof'), type: 'fof' }); // FOF
 }
 
 
